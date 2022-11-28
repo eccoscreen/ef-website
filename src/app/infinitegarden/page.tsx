53fb0982426e067e1240fd1f5404476597468746
lassejaco <lasse.jacobsen@ethereum.org>
2022-11-28 11:11:28 -0600

    More vercel fix

--- src/app/infinitegarden/page.tsx
+++ src/app/infinitegarden/page.tsx
@@ -16,7 +16,7 @@ const InfiniteGarden = (props: any) => {
             <p id="main--content--general--paragraph" className="">
               <em>“A finite game is played for the purpose of winning. An infinite game for the purpose of continuing the play.”</em> ~ James P. Carse
               <br></br>
-              Our vision for Ethereum is the Infinite Garden. Ethereum is more than a technology, it is a diverse ecosystem of individuals and organizations that build and grow alongside a protocol. The Ethereum ecosystem wasn't something that was designed by any one individual or organization, but it organically evolved with the support of people who nurture the ecosystem to become more vibrant and diverse.
+              Our vision for Ethereum is the Infinite Garden. Ethereum is more than a technology, it is a diverse ecosystem of individuals and organizations that build and grow alongside a protocol. The Ethereum ecosystem wasn&apos;t something that was designed by any one individual or organization, but it organically evolved with the support of people who nurture the ecosystem to become more vibrant and diverse.
               <br></br>
               Ethereum is a protocol for human coordination. Coordination is a game, but not one that is played to win. Coordination is more like tending a garden, where one works only that the garden may continue to thrive.
               <br></br>
