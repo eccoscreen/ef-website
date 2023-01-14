cbd0a4a14a3de2b058e0508bd1b5a8136c2a590d
lassejaco <lasse.jacobsen@ethereum.org>
2023-01-14 01:53:40 -0800

    Slight nav adjustment in overflow case

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -40,24 +40,26 @@ const Nav = () => {
       </div>
 
       <section className={`${css['main--menu--container']} ${open ? css['open'] : ''}`} id="main--menu--container">
-        <div className={css['main--menu--internal--container']} id="main--menu--internal--container">
-          <div className={`${css['menu--content--container']}`}>
-            <span className={`${css['one']} ${css['menu--content--dot']}`} id="menu--content--text--one"> • </span>
-            <MouseDownLink href="/" className={`${css['two']} ${css['menu--content--text']}`} id="menu--content--text--two"> Home </MouseDownLink>
-            <span className={`${css['three']} ${css['menu--content--dot']}`} id="menu--content--text--three"> • </span>
-            <MouseDownLink href="/infinitegarden" className={`${css['four']} ${css['menu--content--text']}`} id="menu--content--text--four"> Infinite Garden </MouseDownLink>
-            <span className={`${css['five']} ${css['menu--content--dot']}`} id="menu--content--text--five"> • </span>
-            <MouseDownLink href="/ethereum" className={`${css['six']} ${css['menu--content--text']}`} id="menu--content--text--six"> What is Ethereum? </MouseDownLink>
-            <span className={`${css['seven']} ${css['menu--content--dot']}`} id="menu--content--text--seven"> • </span>
-            <MouseDownLink href="/ef" className={`${css['eight']} ${css['menu--content--text']}`} id="menu--content--text--eight"> What is the EF? </MouseDownLink>
-            <span className={`${css['nine']} ${css['menu--content--dot']}`} id="menu--content--text--nine"> • </span>
-            <MouseDownLink href="/philosophy" className={`${css['ten']} ${css['menu--content--text']}`} id="menu--content--text--ten"> EF Philosophy </MouseDownLink>
-            <span className={`${css['eleven']} ${css['menu--content--dot']}`} id="menu--content--text--eleven"> • </span>
-          </div>
-          <div className={css['secondary--links--container']} id="secondary--links--container">
-            <MouseDownLink href="https://blog.ethereum.org/" id="ef--blog--link" target="_blank" rel="noopener noreferrer" className={css['main--menu--secondary--link']}> EF Blog </MouseDownLink>
-            <span className={css['main--menu--secondary--link']}> : </span>
-            <MouseDownLink href="https://ethereum.foundation/report-2022-04.pdf" id="report--link" target="_blank" rel="noopener noreferrer" className={css['main--menu--secondary--link']}> EF Report </MouseDownLink>
+        <div className={css['mask']}>
+          <div className={css['main--menu--internal--container']} id="main--menu--internal--container">
+            <div className={`${css['menu--content--container']}`}>
+              <span className={`${css['one']} ${css['menu--content--dot']}`} id="menu--content--text--one"> • </span>
+              <MouseDownLink href="/" className={`${css['two']} ${css['menu--content--text']}`} id="menu--content--text--two"> Home </MouseDownLink>
+              <span className={`${css['three']} ${css['menu--content--dot']}`} id="menu--content--text--three"> • </span>
+              <MouseDownLink href="/infinitegarden" className={`${css['four']} ${css['menu--content--text']}`} id="menu--content--text--four"> Infinite Garden </MouseDownLink>
+              <span className={`${css['five']} ${css['menu--content--dot']}`} id="menu--content--text--five"> • </span>
+              <MouseDownLink href="/ethereum" className={`${css['six']} ${css['menu--content--text']}`} id="menu--content--text--six"> What is Ethereum? </MouseDownLink>
+              <span className={`${css['seven']} ${css['menu--content--dot']}`} id="menu--content--text--seven"> • </span>
+              <MouseDownLink href="/ef" className={`${css['eight']} ${css['menu--content--text']}`} id="menu--content--text--eight"> What is the EF? </MouseDownLink>
+              <span className={`${css['nine']} ${css['menu--content--dot']}`} id="menu--content--text--nine"> • </span>
+              <MouseDownLink href="/philosophy" className={`${css['ten']} ${css['menu--content--text']}`} id="menu--content--text--ten"> EF Philosophy </MouseDownLink>
+              <span className={`${css['eleven']} ${css['menu--content--dot']}`} id="menu--content--text--eleven"> • </span>
+            </div>
+            <div className={css['secondary--links--container']} id="secondary--links--container">
+              <MouseDownLink href="https://blog.ethereum.org/" id="ef--blog--link" target="_blank" rel="noopener noreferrer" className={css['main--menu--secondary--link']}> EF Blog </MouseDownLink>
+              <span className={css['main--menu--secondary--link']}> : </span>
+              <MouseDownLink href="https://ethereum.foundation/report-2022-04.pdf" id="report--link" target="_blank" rel="noopener noreferrer" className={css['main--menu--secondary--link']}> EF Report </MouseDownLink>
+            </div>
           </div>
         </div>
       </section>
