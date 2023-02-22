c7f976a912022cd0b9ffd61919631d4fb9361d21
ecco screen <mjeffreybryant@gmail.com>
2023-02-22 14:40:33 -0700

    Rotated ether model toward camera.

--- src/components/page/animate.js
+++ src/components/page/animate.js
@@ -157,6 +157,9 @@ export const Animate = (url) => {
                 let geometry = mesh.geometry;
                 let scaleArray = new Float32Array(48000);
 
+                geometry.rotateZ(Math.PI * 0.25);
+                geometry.rotateX(Math.PI * 0.1);
+
                 for (let i = 0; i < scaleArray.length; i++) {
                     scaleArray[i] = 0.01;
                 }
